0eade96e3d7f15a72f03a065c4a23b6d18ee6888
budryte <43143936+budryte@users.noreply.github.com>
2021-10-02 19:31:06 +0100

    header edited and added to all pages

--- Software_Code/index.php
+++ Software_Code/index.php
@@ -1,7 +1,7 @@
-<!DOCTYPE html>
+<?php include 'header.php';?>
 <?php
-session_start();
 require('./php/conn.php');
+session_start();
 
 $login_err = "";
 
@@ -43,84 +43,12 @@ if (isset($_POST['loginRequest'])) {
       $login_err = "Password or username incorrect please try again...";
   }
 }
-
 ?>
 
-<html lang="en">
-
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <!-- Favicon -->
-  <link rel="icon" type="image/png" href="images/logo.png" />
-  <!-- Bootstrap -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
-  <!--Icons-->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
-  <!-- Custom CSS -->
-  <link rel="stylesheet" type="text/css" href="css/style.css" />
-  <title>Theo Health</title>
-</head>
-
-<header>
-  <nav class="navbar navbar-expand-lg">
-    <a href="index.php">
-      <img src="images/theo-logo.png" alt="Logo" width="100" />
-    </a>
-    <div class="collapse navbar-collapse flex-row-reverse" id="navbarNav">
-      <ul class="navbar-nav">
-        <button type="button" class="btn" data-toggle="modal" data-target="#loginModal">
-          Log In
-        </button>
-        <a href="./create-account.php"><button class="btn">Sign up</button></a>
-      </ul>
-    </div>
-  </nav>
-
-  <!-- Log-in pop-up -->
-  <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog-centered modal-dialog">
-      <div class="modal-content">
-        <!-- Pop-up header -->
-        <div class="modal-header">
-          <h4 class="modal-title container text-center">Log In to Theo Health</h4>
-          <button type="button" class="close" data-dismiss="modal">
-            &times;
-          </button>
-        </div>
-        <div class="modal-body">
-          <!-- Login form -->
-          <form class="text-left" method="post">
-            <div class="form-group">
-              <label> Username: </label>
-              <input type="text" class="form-control" name="inputUsername" placeholder="Username" />
-            </div>
-            <div class="form-group">
-              <label> Password: </label>
-              <input type="password" class="form-control" name="inputPassword" placeholder="Password" />
-            </div>
-            <span class="help-block"><?php echo $login_err; ?></span>
-            <div class="container p-0">
-              <button class="btn button-orange w-100" type="submit" name="loginRequest">
-                Log In
-              </button>
-            </div>
-          </form>
-        </div>
-        <div class="container text-center p-0">
-          <p style="color: black">No account?
-            <a href="./create-account.php">Create one
-          </p></a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- End of login pop-up -->
-</header>
 <body class="background">
 <div class="container">
   <div class="row">
-    <div class="col-6 overlay">
+    <div class="col-4">
       <img src="images/theo-logo.png" width="400px" class="img-fluid" alt="Responsive image" />
       <h3>coming soon..</h3>
       <p class="home-text">
@@ -130,7 +58,7 @@ if (isset($_POST['loginRequest'])) {
         <br />
         Sign up to get started!
       </p>
-      <a href="./create-account.php"><button class="btn button-orange">Sign up</button></a>
+      <a href="./create-account.php"><button class="btn button-orange">Sign Up</button></a>
     </div>
   </div>
 </div>
--- Software_Code/my-profile.php
+++ Software_Code/my-profile.php
@@ -1,17 +1,4 @@
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-  <!-- Bootstrap CSS -->
-  <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-    crossorigin="anonymous"
-  />
-  <link rel="stylesheet" type="text/css" href="css/style.css" />
-</head>
+<?php include 'header.php';?>
 
 <body class="background">
   <div class="content-box container w-90 mt-5 p-5 border">
