89c27ce29e8f0894b682f9d9c33aebc3e1dfd2d1
budryte <43143936+budryte@users.noreply.github.com>
2021-10-08 14:15:41 +0100

    upgraded

--- Software_Code/landing-page.php
+++ Software_Code/landing-page.php
@@ -1,37 +1,35 @@
-<?php 
+<?php
 session_start();
 require('php/conn.php');
 include('header.php');
 ?>
 
 <body class="background">
-  <div class="content-box container p-5 mt-5 border">
-    <div class="container">
-      <div class="container text-center">
-        <h2 class="title">Welcome back, <?php echo $_SESSION['firstname'];?> </h2>
-        <div class="row p-5">
-          <div class="col-md">
+  <div class="content-box container border">
+    <h3 class="title">Welcome back, <?php echo $_SESSION['firstname'];?>! </h3>
+    <div class="container d-flex justify-content-center">
+        <div class="row">
+          <div class="col text-center border" id="homeItem">
             <i class="fas fa-user-circle fa-7x"></i>
             <br></br>
-            <a href="my-profile.php"><button class="btn btn-outline-dark w-50">My Profile</button></a>
+            <a href="my-profile.php"><button class="btn button-orange">My Profile</button></a>
           </div>
           <?php if ($_SESSION['role'] == "physiotherapist") {
-            echo '<div class="col-md">
+            echo '<div class="col text-center border" id="homeItem">
             <i class="fas fa-users fa-7x"></i>
             <br></br>
-            <a href="clients.php"><button class="btn btn-outline-dark w-50">My Clients</button></a>
+            <a href="clients.php"><button class="btn button-orange">My Clients</button></a>
           </div>';
           } ?>
           <?php if ($_SESSION['role'] == "athlete") {
-            echo '<div class="col-md">
+            echo '<div class="col text-center border" id="homeItem">
             <i class="fas fa-chart-line fa-7x"></i>
             <br></br>
-            <a href="progress-overview.php"><button class="btn btn-outline-dark w-50">Progress</button></a>
+            <a href="progress-overview.php"><button class="btn button-orange">Progress</button></a>
           </div>';
           } ?>
 
         </div>
-      </div>
     </div>
   </div>
 </body>
