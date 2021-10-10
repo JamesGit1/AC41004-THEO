906b7c271bc9176cb9d72a9050615f90d08ba14c
betho <bethogilvy4@gmail.com>
2021-10-10 22:17:50 +0100

    Added Functionality to the Post Analysis Page
    Post analysis page is now functional.

--- Software_Code/getfile.js
+++ Software_Code/getfile.js
@@ -3,6 +3,29 @@ $(document).ready(function() {
 
         $('#files').bind('change', handleFileSelect);
     });
+    var container = document.querySelector('.heatmap');
+    var viewValue ="1";
+    var table = document.getElementById("dataTable");
+    function selectChange() {
+        viewValue = document.getElementById("viewSelector").value;
+
+        if (viewValue == "1") {
+            table.rows[0].cells[1].textContent = "Left Quad";
+            document.getElementById("legsimg").src = "./images/legsfcolored.png";
+        }
+        else if (viewValue == "2") {
+            table.rows[0].cells[1].textContent = "Right Quad";
+            document.getElementById("legsimg").src = "./images/legsfcolored.png";
+        }
+        else if (viewValue == "3") {
+            table.rows[0].cells[1].textContent = "Left Hamstring";
+            document.getElementById("legsimg").src = "./images/legsbcolored.png";
+        }
+        else if (viewValue == "4") {
+            table.rows[0].cells[1].textContent = "Right Hamstring";
+            document.getElementById("legsimg").src = "./images/legsbcolored.png";
+        }
+    }
 
     function handleFileSelect(evt) {
       var files = evt.target.files; // FileList object
@@ -16,7 +39,6 @@ $(document).ready(function() {
       reader.readAsText(file);
       reader.onload = function(event){
         var csv = event.target.result;
-        var test = "test";
         const values = [];
         const timestamps = [];
         var data = $.csv.toArrays(csv);
@@ -39,10 +61,11 @@ $(document).ready(function() {
           //html += '<td>' + test + '\r\n';
         }
         values.splice(0,1);
-        // document.getElementById("arrPrint").innerHTML = values;
+        console.log(values);
+        //document.getElementById("arrPrint").innerHTML = values;
         timestamps.splice(0,1);
         //document.getElementById("arrPrint").innerHTML = timestamps;
-
+        var eee = 0;
         //start of all heatmap code
         function AnimationPlayer(options) {
             this.heatmap = options.heatmap;
@@ -68,27 +91,23 @@ $(document).ready(function() {
                     this.isPlaying = !this.isPlaying;
                 }.bind(this);
                 playButton.innerText = 'play';
-
                 this.wrapperEl.appendChild(playButton);
-
                 var events = document.createElement('div');
                 events.className = 'heatmap-timeline';
                 events.innerHTML = '';
-
                 for (var i = 0; i < dataLen; i++) {
-
                     var xOffset = 100 / (dataLen - 1) * i;
-
                     var ev = document.createElement('div');
                     ev.className = 'time-point';
                     ev.style.left = xOffset + '%';
-
                     ev.onclick = (function (i) {
                         return function () {
                             this.isPlaying = false;
                             this.stop();
                             this.setFrame(i);
-
+                            console.log(values[i]);
+                            table.rows[1].cells[1].textContent = values[i];
+                            table.rows[1].cells[0].textContent = timestamps[i];
                         }.bind(this);
                     }.bind(this))(i);
 
@@ -101,10 +120,12 @@ $(document).ready(function() {
             play: function () {
                 var dataLen = this.data.length;
                 this.playButton.innerText = 'pause';
-
                 this.interval = setInterval(function () {
+                  table.rows[1].cells[1].textContent = values[this.currentFrame];
+                  table.rows[1].cells[0].textContent = timestamps[this.currentFrame];
+
                     this.setFrame(++this.currentFrame % dataLen);
-                }.bind(this), this.animationSpeed)
+                }.bind(this), this.animationSpeed);
 
             },
             stop: function () {
@@ -119,9 +140,7 @@ $(document).ready(function() {
                 // Rewrite myself
                 var timePoints = document.getElementsByClassName('time-point');
                 for (var i = 0; i < timePoints.length; i++) {
-
                     timePoints[i].classList.remove('active');
-
                 }
                 timePoints[frame].classList.add('active');
             },
@@ -148,6 +167,8 @@ $(document).ready(function() {
         // generate some heatmap data objects
         for (var i = 0; i < values.length; i++) {
             animationData.push(createAnimationSlide());
+
+
         }
 
         function createAnimationSlide() {
@@ -155,27 +176,113 @@ $(document).ready(function() {
                 // only container is required, the rest will be defaults
                 container: document.querySelector('.heatmap')
 
-            }
+            };
 
             var heatmapInstance = h337.create(config);
 
             // now generate some random data
             var points = [];
-            var max = 1000;
+            var max = 1024
+            var containerWidth = container.offsetWidth;
+          if (viewValue == "1") {
+                // Left Side we plant 22 points along a line and change the radius a bit to make them fit better
+                // First set starting x and y coords and values for all datapoints
+                var x = 0.408;
+                var y = 0.199;
+                var sensorValue =values[i]; // Extract value from json object for each sensor
+                for (let i = 0; i < 22; i++) {
+                    var newRadius = Math.floor(0.023 * container.offsetWidth);
+                    if (i > 16) {
+                        newRadius = Math.floor(0.018 * container.offsetWidth);
+                    }
+                    else if (i > 14) {
+                        newRadius = Math.floor(0.021 * container.offsetWidth);
+                    }
+                    var point = {
+                        x: Math.floor(x * containerWidth), // We got to move the heatmaps with the width of the container changes
+                        y: Math.floor(y * containerWidth),
+                        value: sensorValue,
+                        radius: newRadius // When we get lower on the leg change the radius
+                    };
+                    points.push(point);
+                    x += 0.001; // Move right and down
+                    y += 0.0048;
+                }}
+                else if(viewValue =="2"){var x = 0.59;
+                var y = 0.199;
+                var sensorValue =values[i]; // Extract value from json object for each sensor
+                for (let i = 0; i < 22; i++) {
+                    var newRadius = Math.floor(0.023 * container.offsetWidth);
+                    if (i > 16) {
+                        newRadius = Math.floor(0.018 * container.offsetWidth);
+                    }
+                    else if (i > 14) {
+                        newRadius = Math.floor(0.021 * container.offsetWidth);
+                    }
+                    var point = {
+                        x: Math.floor(x * containerWidth), // We got to move the heatmaps with the width of the container changes
+                        y: Math.floor(y * containerWidth),
+                        value: sensorValue,
+                        radius: newRadius // When we get lower on the leg change the radius
+                    };
+                    points.push(point);
+                    x -= 0.001; // Move left and down
+                    y += 0.0048;
+                }}
+                else if (viewValue == "3") {
+                    // Back very similar to front, no change in radius
+                    // Left
+                    var x = 0.44;
+                    var y = 0.205;
+                    var sensorValue =values[i]; // Extract value from json object for each sensor
+                    for (let i = 0; i < 28; i++) {
+                        var newRadius = Math.floor(0.016 * container.offsetWidth);
+                        var point = {
+                            x: Math.floor(x * containerWidth), // We got to move the heatmaps with the width of the container changes
+                            y: Math.floor(y * containerWidth),
+                            value: sensorValue,
+                            radius: newRadius // When we get lower on the leg change the radius
+                        };
+                        points.push(point);
+                        x += 0.0005; // Move right and down
+                        y += 0.0048;
+                    }}
+                    else if (viewValue =="4"){
+                      var x = 0.562;
+                      var y = 0.205;
+                      var sensorValue =values[i]; // Extract value from json object for each sensor
+                      for (let i = 0; i < 28; i++) {
+                          var newRadius = Math.floor(0.016 * container.offsetWidth);
+                          var point = {
+                              x: Math.floor(x * containerWidth), // We got to move the heatmaps with the width of the container changes
+                              y: Math.floor(y * containerWidth),
+                              value: sensorValue,
+                              radius: newRadius // When we get lower on the leg change the radius
+                          };
+                          points.push(point);
+                          x -= 0.0005; // Move left and down
+                          y += 0.0048;
+                      }
+                    }
+
+
+
+
+
+
 
             // Specific points
             // Top Left
 
-
-              var val =values[i];
+            //  var val =values[i];
               //console.log(values[i]);
               //console.log(val);
-              var point = {
-                  x: 160,
-                  y: 470,
-                  value: val
-                };
-            points.push(point);
+        //      var point = {
+            //      x: 160,
+            //      y: 470,
+              //    value: val
+            //    };
+          //  points.push(point);
 
 
         //  console.log(points);
--- Software_Code/review-session.php
+++ Software_Code/review-session.php
@@ -1,14 +1,12 @@
 <?php
 session_start();
 include('header.php');
+include('./php/conn.php');
+include('./php/commentSubmitter.php');
 ?>
+
 <head>
   <style>
-    label {
-      font: white;
-      color: white;
-    }
-
     #share {
       display: none;
       float: right;
@@ -21,184 +19,173 @@ include('header.php');
       float: right;
     }
 
+        /* animation player css */
+        .timeline-wrapper {
+
+          top: 0px;
+          left: 0px;
+          right: 10px;
+          height: 30px;
+          background: white;
+          transition: 1s ease all;
+          border-radius: 4px;
+          box-shadow: 0 1px 5px rgba(0, 0, 0, 0.65);
+        }
+
+        .heatmap-timeline {
+          position:relative;
+          margin-top: -15px;
+          right: 15px;
+          left: 80px;
+          height: 100%;
+        }
+
+        .heatmap-timeline .line {
+          left: 0;
+          right: 0;
+          top: 0px;
+          height: 2px;
+          background: #d7d7d7;
+        }
+
+        .heatmap-timeline .time-point.active {
+          background: black;
+        }
+
+        .heatmap-timeline .time-point {
+          position:absolute;
+          background: white;
+          border: 2px solid #272727;
+          width: 8px;
+          height: 8px;
+          border-radius: 100%;
+          cursor: pointer;
+          top: 0px;
+          transform: translateX(-50%) translateY(-50%);
+        }
+
+        .heatmap-timeline .time-point:hover {
+          box-shadow: 0 0 5px black;
+        }
+
+        .timeline-wrapper button {
+          outline: none;
+          color: black;
+          background: #f2f2f2;
+          width: 65px;
+          height: 100%;
+          cursor: pointer;
+          border: none;
+          text-transform: uppercase;
+          border-top-left-radius: 3px;
+          border-bottom-left-radius: 3px;
+        }
+
+        .heatmap-timeline .time-point.active {
+          background: black;
+        }
+
     .animation-wrapper {
       /* position: relative;
       align-items: center; */
       text-align: center;
     }
 
-    /* animation player css */
-    .timeline-wrapper {
-      /* position: absolute;
-      top: 10px;
-      left: 0px;
-      right: 10px; */
-      margin-top: 10px;
-      margin-bottom: 10px;
-      height: 30px;
-      background: white;
-      transition: 1s ease all;
-      border-radius: 4px;
-      box-shadow: 0 1px 5px rgba(0, 0, 0, 0.65);
+    #legsimg {
+      width: 30%;
+      height: auto;
     }
-
-    .heatmap-timeline {
-      position: absolute;
-      top: 0;
-      right: 15px;
-      left: 80px;
-      height: 100%;
-    }
-
-    .heatmap-timeline .line {
-      position: absolute;
-      left: 0;
-      right: 0;
-      top: 15px;
-      height: 2px;
-      background: #d7d7d7;
-    }
-
-    .heatmap-timeline .time-point.active {
-      background: black;
-    }
-
-    .heatmap-timeline .time-point {
-      position: absolute;
-      background: white;
-      border: 2px solid #272727;
-      width: 8px;
-      height: 8px;
-      border-radius: 100%;
-      cursor: pointer;
-      top: 15px;
-      transform: translateX(-50%) translateY(-50%);
-    }
-
-    .heatmap-timeline .time-point:hover {
-      box-shadow: 0 0 5px black;
-    }
-
-    .timeline-wrapper button {
-      position: absolute;
-      outline: none;
-      color: black;
-      background: #f2f2f2;
-      width: 65px;
-      height: 100%;
-      cursor: pointer;
-      border: none;
-      text-transform: uppercase;
-      border-top-left-radius: 3px;
-      border-bottom-left-radius: 3px;
-    }
-
-    .heatmap-timeline .time-point.active {
-      background: black;
-    }
-    </style>
+  </style>
 </head>
-
 <body class="background">
-    <div class="content-box container border">
-      <div>
-        <a href="progress-overview.php"><button class="btn mb-3"><i class="fas fa-arrow-left"></i> Client progress</button></a>
-      </div>
-        <h3>Review Session</h3>
-          <select class="form-select form-select" aria-label=".form-select example" onchange="selectChange()" id="viewSelector">
-            <option selected value="f">Front</option>
-            <option value="b">Back</option>
-          </select>
-          <div class="input-group" id="inputs">
-            <input type="file" class="form-control" id="files"  name="files[]" multiple>
-          </div>
-          <!--if sensor selected - show slider, table and legs image  -->
-            <div class="timeline-wrapper"></div>
-
-              <table id="dataTable" class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">Time</th>
-                    <th scope="col">Left Quad</th>
-                    <th scope="col">Right Quad</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">00:00:00</th>
-                    <td>0</td>
-                    <td>0</td>
-                  </tr>
-                </tbody>
-              </table>
-
-            <div class="animation-wrapper">
-                <div class="heatmap">
-                    <img src="images/legsfcolored.png" alt="Legs" id="legsimg">
-                </div>
-            </div>
-
-        <div class="d-flex" id="addFeedbackButton">
-          <button type="button" class="btn button-orange" data-toggle="modal" data-target="#addFeedback">
-            Add Feedback
-            <i class="far fa-comment-alt"></i>
-          </button>
-        </div>
+  <div class="content-box container border">
+    <div>
+      <a href="progress-overview.php"><button class="btn mb-3"><i class="fas fa-arrow-left"></i> Client progress</button></a>
     </div>
+      <h3>Review Session</h3>
+        <div class="container">
+      <select class="form-select form-select" aria-label=".form-select example" onchange="selectChange()" id="viewSelector">
+        <option selected value="1">Sensor 1</option>
+        <option value="2">Sensor 2</option>
+        <option value="3">Sensor 3</option>
+        <option value="4">Sensor 4</option>
+      </select>
+    <div id="inputs" class="clearfix">
+    <input type="file" id="files" name="files[]" multiple />
+  </div>
+  <div class="timeline-wrapper"></div>
+    <table id="dataTable" class="table">
+      <thead>
+        <tr>
+          <th scope="col">Time</th>
+          <th scope="col">Sensor Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">00:00:00</th>
+          <td>0</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 
-    <div class="modal" id="addFeedback">
-        <div class="modal-dialog modal-dialog-centered">
-          <div class="modal-content">
 
-            <div class="modal-header">
-              <h4 class="modal-title">Add feedback</h4>
-                <button type="button" class="modal-close" data-dismiss="modal"><i class="fas fa-times"></i></button>
-            </div>
-
-            <div class="modal-body p-0">
-              <div class="container w-90">
-                <form method="post">
-                  <!-- <div class="form-group">
-                    <label for="currentTimeInput">Time</label>
-                    <input type="text" class="form-control" id="currentTimeInput" aria-describedby="timeHelp" >
-                    <small id="timeHelp" class="form-text text-muted">Current timestamp from sensors</small>
-                  </div> -->
-                  <div class="form-group">
-                    <!-- <label for="textArea">Comment</label> -->
-                    <textarea required class="form-control mb-2" id="textArea" name="inputComment" rows="4" placeholder="Leave comment here.."></textarea>
-                  </div>
-                </form>
-              </div>
-            </div>
-
-            <div class="modal-footer">
-              <button type="button" class="btn" data-dismiss="modal">Cancel</button>
-              <button type="submit" name="commentSubmit" class="btn button-orange">Save Feedback</button>
-            </div>
-
-        </div>
+  <div class="animation-wrapper">
+      <div class="heatmap">
+          <img src="images/legsfcolored.png" alt="Legs" id="legsimg">
       </div>
+
+  </div>
+
+<div class="d-flex" id="addFeedbackButton">
+<button type="button" class="btn button-orange" data-toggle="modal" data-target="#addFeedback">
+  Add Feedback
+  <i class="far fa-comment-alt"></i>
+</button>
+</div>
+</div>
+
+<div class="modal" id="addFeedback">
+<div class="modal-dialog modal-dialog-centered">
+<div class="modal-content">
+
+  <div class="modal-header">
+    <h4 class="modal-title">Add feedback</h4>
+      <button type="button" class="modal-close" data-dismiss="modal"><i class="fas fa-times"></i></button>
+  </div>
+
+  <div class="modal-body p-0">
+    <div class="container w-90">
+      <form method="post">
+        <!-- <div class="form-group">
+          <label for="currentTimeInput">Time</label>
+          <input type="text" class="form-control" id="currentTimeInput" aria-describedby="timeHelp" >
+          <small id="timeHelp" class="form-text text-muted">Current timestamp from sensors</small>
+        </div> -->
+        <div class="form-group">
+          <!-- <label for="textArea">Comment</label> -->
+          <textarea required class="form-control mb-2" id="textArea" name="inputComment" rows="4" placeholder="Leave comment here.."></textarea>
+        </div>
+      </form>
     </div>
-
-    <p id="timestamp"></p>
-    <pre id="arrPrint"></pre>
-
+  </div>
     <!-- Optional JavaScript; choose one of the two! -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script src="https://evanplaice.github.io/jquery-csv/src/jquery.csv.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+      <script src="http://evanplaice.github.io/jquery-csv/src/jquery.csv.min.js"></script>
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
-    <!--  <script src="\build\heatmap.min.js"></script> -->
     <script src="heatmap.min.js"></script>
     <script src="getfile.js"></script>
-    <script src="./retrieveData.js"></script>
-    <!--  <script src="main.js">
+  <!--  <script src="main.js">
   </script> -->
 
     <!-- <script type="text/javascript">
@@ -207,19 +194,4 @@ include('header.php');
             console.log(timePoints);
         });
     </script> -->
-
-    <!-- scripts for modal -->
-    <script
-      src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-      integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
-      crossorigin="anonymous"
-    ></script>
-    <script src="js/script.js"></script>
 </body>
-
-</html>
