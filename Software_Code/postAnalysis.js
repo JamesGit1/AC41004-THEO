ccce6bee3a1947c134372126afecba2f6f80f6ca
betho <bethogilvy4@gmail.com>
2021-10-12 17:38:20 +0100

    Added post analysis script and combined with review page

---
+++ Software_Code/postAnalysis.js
@@ -0,0 +1,273 @@
+$(document).ready(function() {
+
+        $('#files').bind('change', createAnalysis);
+});
+
+function createAnalysis(evt) {
+      var files = evt.target.files; // FileList object
+      var file = files[0];
+      parseData(file);}
+
+
+function percent(partialVal, totalVal) {
+      return (100 * partialVal) / totalVal;}
+
+
+function overInput(values, inputVal){
+  var over = [];
+  for (var i = 0; i < values.length; i++){
+    if (values[i] >= inputVal){
+      over.push(values[i]);}}
+    return over;}
+    function selectChange() {
+        viewValue = document.getElementById("viewSelector").value;
+
+        if (viewValue == "2"  || viewValue =="1") {
+            var view = "front";
+        }
+        else if (viewValue == "3") {
+            var view = "back";
+        }
+    }
+function parseData(file) {
+      var reader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = function(event){
+      var csv = event.target.result;
+        //THIS IS MESSY AND STUPID MAKE IT NICER BETH OF TOMORROW - BETH
+        const values1 = [];
+        const values2 = [];
+        const values3 = [];
+        const values4 = [];
+        const timestamps = [];
+        var content = $.csv.toArrays(csv);
+        var counter1 = 0;
+        var countert = 0;
+        var counter2=0;
+        var counter3=0;
+        var counter4=0;
+        //add every "cell" from the csv into an array based on which column they are in
+        for(var row in content) {
+          for(var item in content[row]) {
+            if (item==0){
+              timestamps[countert] = content[row][item];
+              countert=countert+1;
+            }
+            else if (item==1){
+                values1[counter1] = content[row][item];
+                counter1++;
+              }
+              else if (item==2){
+                values2[counter2] = content[row][item];
+                counter2++;
+              }
+              else if (item==3){
+                values3[counter3] = content[row][item];
+                counter3++;
+              }
+              else if (item==4){
+                values4[counter4] = content[row][item];
+                counter4++;
+              }
+          }}
+
+        //the first value in each array will be the titles from the csv, lets remove them as uneeded
+        values1.splice(0,1);
+        values2.splice(0,1);
+        values3.splice(0,1);
+        values4.splice(0,1);
+        timestamps.splice(0,1);
+
+
+        //get max and mins in most simple way, improve tomorrow
+        var max1 = Math.max(...values1);
+        var min1 = Math.min(...values1);
+        var max2 = Math.max(...values2);
+        var min2 = Math.min(...values2);
+        var max3 = Math.max(...values3);
+        var min3 = Math.min(...values3);
+        var max4 = Math.max(...values4);
+        var min4 = Math.min(...values4);
+
+        var max = [max1,max2,max3,max4];
+        var min = [min1,min2,min3,min4];
+
+
+        //value inputted by user but for now is set
+        var comparisonValue = document.getElementById('targval').value;
+        console.log(comparisonValue);
+        viewValue = document.getElementById("viewSelector").value;
+        console.log(viewValue);
+        if (viewValue=="2"){
+          var sensora=values1;
+          var sensorb=values2;
+        }
+        else{
+          var sensora=values3;
+          var sensorb=values4;
+        }
+
+        var valuesOver1 = overInput(sensora, comparisonValue);
+        var percentage1 = percent(valuesOver1.length, sensora.length);
+
+        var valuesOver2 = overInput(sensorb, comparisonValue);
+        var percentage2 = percent(valuesOver2.length, sensorb.length);
+
+
+
+        const data = {
+          labels: ['Sensor 1', 'Sensor 2','Sensor 3', 'Sensor 4'],
+          datasets: [
+            {
+              label: 'Max Value',
+              data: max,
+              backgroundColor: 'rgb(243,109,33)',
+              stack: 'Stack 0',
+            },
+            {
+              label: 'Min Value',
+              data: min,
+              backgroundColor: 'rgb(2, 151, 162)',
+              stack: 'Stack 1',
+            },
+          ]
+        };
+
+        const config = {
+          type: 'bar',
+          data: data,
+          options: {
+            responsive: true,
+            plugins: {
+              legend: {
+              },
+              position: 'top',
+              title: {
+                display: true,
+                text: 'Min/max value comparison',
+                // size: 24,
+              }
+            }
+          }
+        };
+
+        const data2 = {
+          labels: ['Below target', 'Above target'],
+          datasets: [
+            {
+              data: [percentage1, 100-percentage1],
+              label: 'Below target',
+              backgroundColor: ['rgb(243,109,33)', 'rgb(2, 151, 162)'],
+            },
+          ]
+        };
+
+        const config2 = {
+          type: 'pie',
+          data: data2,
+          options: {
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'top',
+              },
+              title: {
+                display: true,
+                text: 'Target Reach - Left Leg'
+              }
+            }
+          }
+        };
+
+
+
+
+        const data3 = {
+          labels: ['Below target', 'Above target'],
+          datasets: [
+            {
+              label: 'Below target',
+              data: [percentage2, 100-percentage2],
+              backgroundColor: ['rgb(243,109,33)', 'rgb(2, 151, 162)'],
+            },
+          ]
+        };
+
+        const config3 = {
+          type: 'pie',
+          data: data3,
+          options: {
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'top',
+              },
+              title: {
+                display: true,
+                text: 'Target Reach - Right Leg'
+              }
+            }
+          }
+        };
+
+
+
+
+        const data4 = {
+        labels: timestamps,
+        datasets: [
+          {
+            label: 'Left Leg',
+            data: sensora,
+            borderColor: 'rgb(243,109,33)',
+            backgroundColor: 'rgb(243,109,33)',
+          },
+          {
+            label: 'Right Leg',
+            data: sensorb,
+            borderColor: 'rgb(2, 151, 162)',
+            backgroundColor: 'rgb(2, 151, 162)',
+          }
+        ]
+      };
+
+        const config4 = {
+          type: 'line',
+          data: data4,
+          options: {
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'top',
+              },
+              title: {
+                display: true,
+                text: 'Line graph'
+              }
+            }
+          },
+        };
+
+        var myChart = new Chart(
+          document.getElementById('myChart'),
+          config
+        );
+
+        var myPieChart = new Chart(
+          document.getElementById('myPieChart'),
+          config2
+        );
+
+        var myAnotherPieChart = new Chart(
+          document.getElementById('myPieChart2'),
+          config3
+        );
+
+        var myLineChart = new Chart(
+          document.getElementById('myLineChart'),
+          config4
+        );
+
+
+
+      }}
--- Software_Code/review-session.php
+++ Software_Code/review-session.php
@@ -19,82 +19,6 @@ include('./php/commentSubmitter.php');
       float: right;
     }
 
-        /* animation player css */
-        .timeline-wrapper {
-
-          top: 0px;
-          left: 0px;
-          right: 10px;
-          height: 30px;
-          background: white;
-          transition: 1s ease all;
-          border-radius: 4px;
-          box-shadow: 0 1px 5px rgba(0, 0, 0, 0.65);
-        }
-
-        .heatmap-timeline {
-          position:relative;
-          margin-top: -15px;
-          right: 15px;
-          left: 80px;
-          height: 100%;
-        }
-
-        .heatmap-timeline .line {
-          left: 0;
-          right: 0;
-          top: 0px;
-          height: 2px;
-          background: #d7d7d7;
-        }
-
-        .heatmap-timeline .time-point.active {
-          background: black;
-        }
-
-        .heatmap-timeline .time-point {
-          position:absolute;
-          background: white;
-          border: 2px solid #272727;
-          width: 8px;
-          height: 8px;
-          border-radius: 100%;
-          cursor: pointer;
-          top: 0px;
-          transform: translateX(-50%) translateY(-50%);
-        }
-
-        .heatmap-timeline .time-point:hover {
-          box-shadow: 0 0 5px black;
-        }
-
-        .timeline-wrapper button {
-          outline: none;
-          color: black;
-          background: #f2f2f2;
-          width: 65px;
-          height: 100%;
-          cursor: pointer;
-          border: none;
-          text-transform: uppercase;
-          border-top-left-radius: 3px;
-          border-bottom-left-radius: 3px;
-        }
-
-        .heatmap-timeline .time-point.active {
-          background: black;
-        }
-
-    .animation-wrapper {
-      /* position: relative;
-      align-items: center; */
-      text-align: center;
-    }
-
-    #legsimg {
-      width: 30%;
-      height: auto;
-    }
 
     #pieChart {
       max-width: 340px;
@@ -130,28 +54,7 @@ include('./php/commentSubmitter.php');
         <input type="file" class="form-control" id="files"  name="files[]" multiple>
       </div>
 
-      <!-- <div class="timeline-wrapper"></div>
-        <table id="dataTable" class="table">
-          <thead>
-            <tr>
-              <th scope="col">Time</th>
-              <th scope="col">Sensor Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">00:00:00</th>
-              <td>0</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
 
-    <div class="animation-wrapper">
-        <div class="heatmap">
-            <img src="images/legsfcolored.png" alt="Legs" id="legsimg">
-        </div>
-    </div> -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
       <!-- Bar char -->
@@ -169,7 +72,7 @@ include('./php/commentSubmitter.php');
         <div class="input-group-prepend">
           <span class="input-group-text">Target Value</span>
         </div>
-        <input type="number" class="form-control" aria-label="Target">
+         <input type="text"  placeholder="Enter Target Value" id="targval"/>
         <div class="input-group-append">
           <button class="btn" type="button">Apply</button>
         </div>
@@ -188,155 +91,8 @@ include('./php/commentSubmitter.php');
         <canvas id="myLineChart"></canvas>
       </div>
 
-    <script>
-      const data = {
-        labels: ['Sensor 1', 'Sensor 2','Sensor 3', 'Sensor 4'],
-        datasets: [
-          {
-            label: 'Max Value',
-            data: [510, 120, 400, 80],
-            backgroundColor: 'rgb(243,109,33)',
-            stack: 'Stack 0',
-          },
-          {
-            label: 'Min Value',
-            data: [40, 10, 250, 20],
-            backgroundColor: 'rgb(2, 151, 162)',
-            stack: 'Stack 1',
-          },
-        ]
-      };
 
-      const config = {
-        type: 'bar',
-        data: data,
-        options: {
-          responsive: true,
-          plugins: {
-            legend: {
-              position: 'top',
-            },
-            title: {
-              display: true,
-              text: 'Min/max value comparison',
-              // size: 24,
-            }
-          }
-        }
-      };
 
-      const data2 = {
-        labels: ['Below target', 'Above target'],
-        datasets: [
-          {
-            label: 'Below target',
-            data: [510, 40],
-            backgroundColor: ['rgb(243,109,33)', 'rgb(2, 151, 162)'],
-          },
-        ]
-      };
-
-      const config2 = {
-        type: 'pie',
-        data: data2,
-        options: {
-          responsive: true,
-          plugins: {
-            legend: {
-              position: 'top',
-            },
-            title: {
-              display: true,
-              text: 'Target Reach - Left Leg'
-            }
-          }
-        }
-      };
-
-      const data3 = {
-        labels: ['Below target', 'Above target'],
-        datasets: [
-          {
-            label: 'Below target',
-            data: [230, 100],
-            backgroundColor: ['rgb(243,109,33)', 'rgb(2, 151, 162)'],
-          },
-        ]
-      };
-
-      const config3 = {
-        type: 'pie',
-        data: data3,
-        options: {
-          responsive: true,
-          plugins: {
-            legend: {
-              position: 'top',
-            },
-            title: {
-              display: true,
-              text: 'Target Reach - Right Leg'
-            }
-          }
-        }
-      };
-
-      const data4 = {
-      labels: ['10:51', '10:52', '10:52', '10:53', '10:54', '10:55', '10:56', '10:57', '10:58', '10:59', '11:00', '11:01', '11:02'],
-      datasets: [
-        {
-          label: 'Sensor 1',
-          data: [10, 25, 84, 200, 451, 320, 471, 821, 214, 140, 48, 98, 752, 701, 25],
-          borderColor: 'rgb(243,109,33)',
-          backgroundColor: 'rgb(243,109,33)',
-        },
-        {
-          label: 'Sensor 2',
-          data: [714, 521, 653, 698, 745, 546, 223, 353, 21, 41, 60, 20, 63, 687, 74],
-          borderColor: 'rgb(2, 151, 162)',
-          backgroundColor: 'rgb(2, 151, 162)',
-        }
-      ]
-    };
-
-      const config4 = {
-        type: 'line',
-        data: data4,
-        options: {
-          responsive: true,
-          plugins: {
-            legend: {
-              position: 'top',
-            },
-            title: {
-              display: true,
-              text: 'Line graph'
-            }
-          }
-        },
-      };
-
-      var myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-      );
-
-      var myPieChart = new Chart(
-        document.getElementById('myPieChart'),
-        config2
-      );
-
-      var myAnotherPieChart = new Chart(
-        document.getElementById('myPieChart2'),
-        config3
-      );
-
-      var myLineChart = new Chart(
-        document.getElementById('myLineChart'),
-        config4
-      );
-
-  </script>
 </div>
 
   <div class="d-flex" id="addFeedbackButton">
@@ -359,13 +115,9 @@ include('./php/commentSubmitter.php');
   <div class="modal-body p-0">
     <div class="container w-90">
       <form method="post">
-        <!-- <div class="form-group">
-          <label for="currentTimeInput">Time</label>
-          <input type="text" class="form-control" id="currentTimeInput" aria-describedby="timeHelp" >
-          <small id="timeHelp" class="form-text text-muted">Current timestamp from sensors</small>
-        </div> -->
+
         <div class="form-group">
-          <!-- <label for="textArea">Comment</label> -->
+
           <textarea required class="form-control mb-2" id="textArea" name="inputComment" rows="4" placeholder="Leave comment here.."></textarea>
         </div>
         <div class="modal-footer">
@@ -375,28 +127,15 @@ include('./php/commentSubmitter.php');
       </form>
     </div>
   </div>
-    <!-- Optional JavaScript; choose one of the two! -->
+
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
       <script src="http://evanplaice.github.io/jquery-csv/src/jquery.csv.min.js"></script>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-    <script src="heatmap.min.js"></script>
-    <script src="getfile.js"></script>
-  <!--  <script src="main.js">
-  </script> -->
 
-    <!-- <script type="text/javascript">
-        window.addEventListener('load', (event) => {
-            var timePoints = document.getElementsByClassName('heatmap-timeline');
-            console.log(timePoints);
-        });
-    </script> -->
+    <script src="postAnalysis.js"></script>
+
 </body>
