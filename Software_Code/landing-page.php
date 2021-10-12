eba4293d3440b2dd1ba86adfc599a840a7d33354
Daniel Denley <daniel-denley@live.co.uk>
2021-10-12 22:47:50 +0100

    CSS changes to landing page

--- Software_Code/landing-page.php
+++ Software_Code/landing-page.php
@@ -5,24 +5,24 @@ include('header.php');
 ?>
 
 <body class="background">
-  <div class="content-box container border">
+  <div class="content-box-myprofile container">
     <h3 class="title">Welcome back, <?php echo $_SESSION['firstname'];?>! </h3>
     <div class="container d-flex justify-content-center">
         <div class="row">
-          <div class="col text-center border" id="homeItem">
+          <div class="col text-center" id="homeItem">
             <i class="fas fa-user-circle fa-7x"></i>
             <br></br>
             <a href="my-profile.php"><button class="btn button-orange">My Profile</button></a>
           </div>
           <?php if ($_SESSION['role'] == "physiotherapist") {
-            echo '<div class="col text-center border" id="homeItem">
+            echo '<div class="col text-center" id="homeItem">
             <i class="fas fa-users fa-7x"></i>
             <br></br>
             <a href="clients.php"><button class="btn button-orange">My Clients</button></a>
           </div>';
           } ?>
           <?php if ($_SESSION['role'] == "athlete") {
-            echo '<div class="col text-center border" id="homeItem">
+            echo '<div class="col text-center" id="homeItem">
             <i class="fas fa-chart-line fa-7x"></i>
             <br></br>
             <a href="progress-overview.php"><button class="btn button-orange">Progress</button></a>
