e7c4937747f81a6eb4df250dbf5586607f85e038
James Shaw <jamesshaw112@sky.com>
2021-10-13 14:38:49 +0100

    adding physio signup back

--- Software_Code/create-account.php
+++ Software_Code/create-account.php
@@ -1,20 +1,35 @@
 <?php
 session_start();
-include('header.php');
 require('./php/conn.php');
 include('php/createAccount.php');
+include('header.php');
 ?>
 
 <body class="background">
   <div class="content-box container border">
     <div class="container text-center">
       <a href="./index.php">
-      <img src="images/theo-logo2.png" alt="Theo Health Logo" class="img-fluid" id="createAccountLogo" /></a>
+        <img src="images/theo-logo2.png" alt="Theo Health Logo" class="img-fluid" id="createAccountLogo" /></a>
     </div>
-      <h3>Create your account</h3>
-      <p>*For now you require a code from an advisor to access theo health</p>
+    <h3>Create your account</h3>
+    <div class="container">
+      <div class="row">
+        <div class="col mb-2">
+          <a href="./create-account_physio.php">I am a physiotherapist</a>
+        </div>
+        <div class="row">
+          <div class="col">
+            <p>To sign up as an athlete you require an activation code from a physiotherapist</p>
+          </div>
+        </div>
+      </div>
+
       <form class="" method="post">
-        <div class="form-group w-auto">
+        <div class="form-group">
+          <label>Code: </label>
+          <input type="int" class="form-control" name="code" value="" placeholder="Code..." required maxlength="11" />
+        </div>
+        <div class="form-group">
           <label>Username: </label>
           <input type="text" class="form-control" name="inputusername" placeholder="Username..." required maxlength="45" />
         </div>
@@ -22,15 +37,11 @@ include('php/createAccount.php');
           <label>Password: </label>
           <input type="password" class="form-control" name="password1" placeholder="Password..." required />
         </div>
-        <div class="form-group">
+        <div class="form-group mb-2">
           <label>Confirm Password: </label>
           <input type="password" class="form-control" name="password2" placeholder="Confirm Password..." required />
         </div>
-        <div class="form-group mb-3">
-          <label>Code: </label>
-          <input type="int" class="form-control" name="code" value="" placeholder="Code..." required maxlength="11" />
-        </div>
         <button class="btn button-orange mt-2 btn-lg" type="submit" name="submitAccount">Register</button>
       </form>
-  </div>
+    </div>
 </body>
---
+++ Software_Code/create-account_physio.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+require('./php/conn.php');
+include('php/createAccount_physio.php');
+include('header.php');
+?>
+
+<body class="background">
+  <div class="content-box container border">
+    <div class="container text-center">
+      <a href="./index.php">
+        <img src="images/theo-logo2.png" alt="Theo Health Logo" class="img-fluid" id="createAccountLogo" /></a>
+    </div>
+    <h3>Create your account</h3>
+    <form class="" method="post">
+      <div class="form-group">
+        <label>Account Type: </label>
+        <select class="form-select form-select-md mb-3" aria-label=".form-select-lg example" name="role">
+          <!-- <option value="none" selected>Please select...</option> -->
+          <option selected value="physiotherapist">Physiotherapist</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label>First Name: </label>
+        <input type="text" class="form-control" name="firstname" value="<?php if (isset($firstname)) echo $firstname; ?>" placeholder="First Name..." required maxlength="45" />
+      </div>
+      <div class="form-group">
+        <label>Last Name: </label>
+        <input type="text" class="form-control" name="lastname" value="<?php if (isset($lastname)) echo $lastname; ?>" placeholder="Last Name..." required maxlength="45" />
+      </div>
+      <div class="form-group">
+        <label>Email: </label>
+        <input type="email" class="form-control" name="email" value="<?php if (isset($email)) echo $email; ?>" placeholder="Email..." required maxlength="200" />
+      </div>
+      <div class="form-group w-auto">
+        <label>Username: </label>
+        <input type="text" class="form-control" name="inputusername" value="<?php if (isset($inputusername)) echo $inputusername; ?>" placeholder="Username..." required maxlength="45" />
+      </div>
+      <div class="form-group">
+        <label>Password: </label>
+        <input type="password" class="form-control" name="password1" placeholder="Password..." required />
+      </div>
+      <div class="form-group mb-2">
+        <label>Confirm Password: </label>
+        <input type="password" class="form-control" name="password2" placeholder="Confirm Password..." required />
+        <span class="help-block"><?php if (isset($username_err)) echo $username_err; ?></span>
+      </div>
+      <button class="btn button-orange mt-2 btn-lg" type="submit" name="submitAccount">Register</button>
+    </form>
+  </div>
+</body>
